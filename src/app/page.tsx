0b6a952139fa9eab0bce4d586cf1e90b79636520
johanrussouw <jrussouw610505@gmail.com>
2025-10-25 15:11:58 +0200

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import db from "@/lib/db";
 
 export default async function Home() {
   const user = await db.user.findFirst();
-  return <>{JSON.stringify(user)}</>;
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
 }
 // change
